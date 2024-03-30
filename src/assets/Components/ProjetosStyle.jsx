a4af54a4d7eb20b28e053283e2b267e199a81c0c
robertapdm <robertafarmaco@gmail.com>
2024-03-30 11:25:24 -0300

    editing page of projects

---
+++ src/assets/Components/ProjetosStyle.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+export const Div = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+export const Card = styled.div`
+width: 60%;
+height: 70vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+export const P = styled.p`
+width: 50%;
+max-width: 20%;
+
+margin: 0 0 0 50px;
+`
+export const Projetos = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-evenly;
+align-items: flex-start;
+`
+export const Img = styled.img`
+width: 300px;
+`
+export const ImgFooter = styled.img`
+width: 100%;
+height: 150px;
+object-fit: fill;
+`
